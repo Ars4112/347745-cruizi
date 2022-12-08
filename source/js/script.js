a7aa04c323d43aa0ff12537e6f834f6930a7b7a3
Ars4112 <Ars4112@gmail.com>
2022-12-08 23:01:43 +0300

    добавляет футер

--- source/js/script.js
+++ source/js/script.js
@@ -4,7 +4,7 @@ if (viewport <= 767) {
   const header = document.querySelector('.header');
   const headerNav = header.querySelector('.header__nav');
   const menuMobile = header.querySelector('.menu-mobile');
-  const buttonOpened = headerNav.querySelector('.header__button');
+  const buttonOpened = headerNav.querySelector('.nav__button');
   const buttonCloced = menuMobile.querySelector('.menu-mobile__button');
   const menuMobileNav = menuMobile.querySelector('.menu-mobile__nav');
 
@@ -12,13 +12,13 @@ if (viewport <= 767) {
   menuMobileNav.classList.add('menu-mobile__nav--position');
   menuMobile.classList.remove('menu-mobile--visible');
   menuMobile.classList.add('menu-mobile--hidden');
-  headerNav.classList.remove('header__nav--hidden');
+  headerNav.classList.remove('nav--hidden');
   buttonCloced.style.display = 'block';
 
   [buttonOpened, buttonCloced].forEach((elem) => {
     elem.addEventListener('click', () => {
       menuMobile.classList.toggle('menu-mobile--hidden');
-      headerNav.classList.toggle('header__nav--hidden');
+      headerNav.classList.toggle('nav--hidden');
     });
   });
 }
